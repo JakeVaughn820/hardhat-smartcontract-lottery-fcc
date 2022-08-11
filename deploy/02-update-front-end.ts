25eeb905030c7dbfc9db3910dc5bf739a7db36a2
jakevaughn820 <jakevaughn820@gmail.com>
2022-08-11 17:12:53 -0500

    Added update to .env example and chainId to mock

--- deploy/02-update-front-end.ts
+++ deploy/02-update-front-end.ts
@@ -5,18 +5,19 @@ import { HardhatRuntimeEnvironment } from "hardhat/types"
 
 const updateUI: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { network, ethers } = hre
-    const chainId = "31337"
+    const chainId = network.config.chainId!
 
+    console.log(process.env.UPDATE_FRONT_END)
     if (process.env.UPDATE_FRONT_END) {
         console.log("Writing to front end...")
-        const fundMe = await ethers.getContract("Raffle")
+        const raffle = await ethers.getContract("Raffle")
         const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractsFile, "utf8"))
         if (chainId in contractAddresses) {
-            if (!contractAddresses[network.config.chainId!].includes(fundMe.address)) {
-                contractAddresses[network.config.chainId!].push(fundMe.address)
+            if (!contractAddresses[network.config.chainId!].includes(raffle.address)) {
+                contractAddresses[network.config.chainId!].push(raffle.address)
             }
         } else {
-            contractAddresses[network.config.chainId!] = [fundMe.address]
+            contractAddresses[chainId] = [raffle.address]
         }
         fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses))
         console.log("Front end written!")
